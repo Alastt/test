d55a7426a2f8c91410eaf29ffe36daf15320a61b
alast <inggiomtzg@hotmail.com>
2016-09-02 22:58:50 -0500

    1

---
+++ resources/lang/en/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'La contraseña debe ser minimo de 6 caracteres y coincidir con la confirmación.',
+    'reset' => 'Tu contraseña ha sido reiniciada',
+    'sent' => 'Hemos enviado un mail con la liga a la pagina de reinicio de contraseña!',
+    'token' => 'El token de reinicio de contraseña es invalido.',
+    'user' => "No hay ningun usuario con ese e-mail.",
+
+];
